c7bf59083c6f1a0406e4cb81fe17cd972046705f
Dirtyhabitsonline <dirtyhabitscasino@icloud.com>
2024-05-28 06:12:43 -0500

    Add project files.

---
+++ DirtyHabits/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DirtyHabits
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ DirtyHabits/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DirtyHabits
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
